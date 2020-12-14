a263de27f836a27191f1a5098d0345f9874d2052
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2020-12-14 22:52:02 +0100

    added an extra script that's ensure program could be re-written by OTA

---
+++ extra_script.py
@@ -0,0 +1,19 @@
+Import("env", "projenv")
+
+# access to global build environment
+print(env)
+
+# access to project build environment (is used source files in "src" folder)
+print(projenv)
+
+#
+# Upload actions
+#
+
+def before_upload(source, target, env):
+    if source[0].get_size() > 490000:
+        raise Exception(f"File size ({source[0].get_size()}) excceds maximum limit to allow OTA") 
+
+print("Current build targets", map(str, BUILD_TARGETS))
+
+env.AddPreAction("upload", before_upload)
