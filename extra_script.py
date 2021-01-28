218018623c2c54bd9582f7238300a8538c8fcc99
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2021-01-28 21:05:28 +0100

    removed unused lines from scripts

--- extra_script.py
+++ extra_script.py
@@ -1,10 +1,10 @@
 Import("env", "projenv")
 
 # access to global build environment
-print(env)
+#print(env)
 
 # access to project build environment (is used source files in "src" folder)
-print(projenv)
+#print(projenv)
 #
 # Upload actions
 #
